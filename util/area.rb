5375fe256051766273d6688c7a6603b81a1fbe3c
Keith Pitt <me@keithpitt.com>
2012-11-18 11:27:49 +1100

    Trying to make wall walking better.

--- util/area.rb
+++ util/area.rb
@@ -14,16 +14,12 @@ class Area < Struct.new(:map, :square)
   end
 
   def kind_of_walkable_points
-    square.area.find_all do |point|
+    square.area.map do |point|
       found = map.find(point)
 
-      if found == nil
-        false
-      elsif point == square.center
-        false
-      else
-        found.walkable? || found.unknown?
+      if found && (found.walkable? || found.unknown?) && found != square.center
+        found
       end
-    end
+    end.compact
   end
 end
